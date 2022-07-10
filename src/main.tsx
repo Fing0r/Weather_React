6b33a2527ca49896276ed83f81bcd06041897ad2
Fin <fantomma91@gmail.com>
2022-07-10 22:46:16 +0700

    add HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style/index.css";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/es/integration/react";
 import { persistStore } from "redux-persist";
 import App from "./App";
@@ -13,10 +13,10 @@ const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
             <PersistGate persistor={persistor} loading={null}>
                 <App />
             </PersistGate>
-        </BrowserRouter>
+        </HashRouter>
     </Provider>,
 );
